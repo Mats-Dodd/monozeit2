a23c2d2efb7653d57a8f4a059812cc96325bd5d5
Mats-Dodd <mats.dodd12@gmail.com>
2025-09-17 19:39:05 -0700

    basic starter kit

--- src/components/editor/extensions.ts
+++ src/components/editor/extensions.ts
@@ -1,4 +1,3 @@
-import StarterKit from "@tiptap/starter-kit"
 import {
   Details,
   DetailsContent,
@@ -9,11 +8,74 @@ import { TaskItem, TaskList } from "@tiptap/extension-list"
 import { Placeholder } from "@tiptap/extensions"
 import Typography from "@tiptap/extension-typography"
 import { DiffExtension } from "@/lib/extensions/diff-extension"
+import { StarterKit } from "@syfxlin/tiptap-starter-kit"
 
 //details needs to be set in slash command
 
 export const extensions = [
-  StarterKit,
+  StarterKit.configure({
+    heading: {
+      levels: [1, 2, 3],
+    },
+    bulletList: {
+      HTMLAttributes: {
+        class: "list-disc",
+      },
+    },
+    orderedList: {
+      HTMLAttributes: {
+        class: "list-decimal",
+      },
+    },
+    blockquote: {
+      HTMLAttributes: {
+        class: "border-l-2 border-gray-300 pl-4",
+      },
+    },
+    code: {
+      HTMLAttributes: {
+        class: "bg-gray-100 p-1 rounded-md",
+      },
+    },
+    codeBlock: {
+      HTMLAttributes: {
+        class: "bg-gray-100 p-1 rounded-md",
+      },
+    },
+    horizontalRule: {
+      HTMLAttributes: {
+        class: "border-t border-gray-300",
+      },
+    },
+    link: {
+      HTMLAttributes: {
+        class: "text-blue-500 hover:text-blue-700",
+      },
+    },
+    table: {
+      HTMLAttributes: {
+        class: "w-full",
+      },
+    },
+    tableCell: {
+      HTMLAttributes: {
+        class: "border border-gray-300 p-2",
+      },
+    },
+    mathBlock: {
+      HTMLAttributes: {
+        class: "math-block",
+      },
+    },
+    mathInline: {
+      HTMLAttributes: {
+        class: "math-inline",
+      },
+    },
+    audio: false,
+    image: false,
+    video: false,
+  }),
   Details,
   DetailsContent,
   DetailsSummary,
